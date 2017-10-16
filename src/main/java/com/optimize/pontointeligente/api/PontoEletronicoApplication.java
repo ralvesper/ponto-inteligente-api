6db1da9e40d03492846f04fbe10687cecf6c094c
Rodrigo Alves Pereira <rodrigo.desenv@gmail.com>
2017-10-15 22:43:11 -0200

    Adicionado EhCache

--- src/main/java/com/optimize/pontointeligente/api/PontoEletronicoApplication.java
+++ src/main/java/com/optimize/pontointeligente/api/PontoEletronicoApplication.java
@@ -3,9 +3,11 @@ package com.optimize.pontointeligente.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableCaching
 public class PontoEletronicoApplication {
 
 	public static void main(String[] args) {
